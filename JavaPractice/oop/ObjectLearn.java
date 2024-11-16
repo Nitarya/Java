16952873ba8530ecacc89135f6bfe74df1763e97
Nitya <niittss27@gmail.com>
2024-11-16 11:47:41 +0530

    Added object

---
+++ JavaPractice/oop/ObjectLearn.java
@@ -0,0 +1,10 @@
+// Creating an object
+
+public class ObjectLearn {
+    int x = 5;
+
+    public static void main(String[] args) {
+        ObjectLearn myObj = new ObjectLearn();
+        System.out.println(myObj.x);
+    }
+}
